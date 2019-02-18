8c7f0a0f6b09fd39b1869c193b3edcbc55058b59
Kevin Kirsche <kev.kirsche@gmail.com>
2019-02-18 09:56:46 -0500

    Fix rust to pass the tests!

--- Problem2-2019-02-17/rust/src/lib.rs
+++ Problem2-2019-02-17/rust/src/lib.rs
@@ -1,5 +1,17 @@
-fn product(x: u32, y: u32) -> u32 {
-    return x * y;
+use std::vec::Vec;
+
+pub fn product_of_vec_except_at_index(list_in: Vec<u32>) -> Vec<u32> {
+    let mut list_out = Vec::new();
+    for index in 0..list_in.len() {
+        let mut list_in_without_index = vec![];
+        list_in_without_index.extend_from_slice(&list_in[..index]);
+        list_in_without_index.extend_from_slice(&list_in[(index + 1)..]);
+
+        let product = list_in_without_index.iter().product();
+        list_out.push(product);
+    }
+
+    return list_out;
 }
 
 #[cfg(test)]
@@ -8,7 +20,10 @@ mod tests {
 
     #[test]
     fn test_product() {
-        let result = product(2, 2);
-        assert_eq!(result, 4);
+        let list_in = vec![1, 2, 3, 4, 5];
+        let expected_out = vec![120, 60, 40, 30, 24];
+
+        let list_out = product_of_vec_except_at_index(list_in);
+        assert_eq!(list_out, expected_out);
     }
 }
