202553a74c930f1a8d318c9baad69f92079e2c4b
Kevin Kirsche <kevin.kirsche@verizon.com>
2019-02-20 11:37:39 -0500

    Add full type hinting

--- Problem4-2019-02-19/python/test_main.py
+++ Problem4-2019-02-19/python/test_main.py
@@ -1,25 +1,26 @@
 from .main import first_positive_missing_integer
+from typing import List
 
 
 def test_first_positive_missing_integer_example_one():
-    list_in = [3, 4, -1, 1]
-    expected_output = 2
+    list_in: List[int] = [3, 4, -1, 1]
+    expected_output: int = 2
 
-    actual_output = first_positive_missing_integer(list_in)
+    actual_output: List[int] = first_positive_missing_integer(list_in)
     assert actual_output == expected_output
 
 
 def test_first_positive_missing_integer_example_two():
-    list_in = [1, 2, 0]
-    expected_output = 3
+    list_in: List[int] = [1, 2, 0]
+    expected_output: int = 3
 
-    actual_output = first_positive_missing_integer(list_in)
+    actual_output: List[int] = first_positive_missing_integer(list_in)
     assert actual_output == expected_output
 
 
 def test_first_positive_missing_integer_example_three():
-    list_in = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, -1, 1]
-    expected_output = 14
+    list_in: List[int] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, -1, 1]
+    expected_output: int = 14
 
-    actual_output = first_positive_missing_integer(list_in)
+    actual_output: List[int] = first_positive_missing_integer(list_in)
     assert actual_output == expected_output
